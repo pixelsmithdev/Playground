37b93218766b81a3be6c800479e1ca8ca85d91ce
Mike Voermans <m.voermans@larsen.com>
2010-12-02 10:40:36 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="language" content="en" />
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="/assets/css/global.css" />
+		<link rel="icon" href="/favicon.ico"/>
+	</head>
+	<body>
+		
+		
+		<script src="/assets/js/global.js"></script>
+	</body>
+</html>
